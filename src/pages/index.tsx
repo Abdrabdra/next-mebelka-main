02248060e3a23b33a785fccabf72661f4dde64e0
Abdrabdra <dico.bb15@mail.ru>
2023-04-23 13:09:26 +0600

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,7 @@ export default function HomePage() {
 export const checkAuth = async () => {
   try {
     const response = await AuthService.refresh();
+    localStorage.setItem("access_token", response.data.access_token);
     console.log("response: ", response);
   } catch (e: any) {
     console.log("suka: ", e.response?.data?.message);
