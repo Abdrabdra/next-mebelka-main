b4c7b9e62ebececae9f9738eaa342761c69c71cd
Abdrabdra <dico.bb15@mail.ru>
2023-04-23 13:04:35 +0600

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,27 @@
+import { DEV_API } from "@api/index";
 import Home from "@components/screens/Home";
 import { MainLayout } from "@layouts/MainLayout";
+import { AuthService } from "@service/auth/auth.service";
+import axios from "axios";
+import { useEffect } from "react";
 
 export default function HomePage() {
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
   return (
     <MainLayout>
       <Home />
     </MainLayout>
   );
 }
+
+export const checkAuth = async () => {
+  try {
+    const response = await AuthService.refresh();
+    console.log("response: ", response);
+  } catch (e: any) {
+    console.log("suka: ", e.response?.data?.message);
+  }
+};
